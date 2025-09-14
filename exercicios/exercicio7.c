ec2d60908ea49f24dfb8f20f161b4647ee13eae0
Neylane <lanevasconcellos1@gmail.com>
2025-09-14 16:05:26 -0400

    Corrigindo  exercicio 7

--- exercicios/exercicio7.c
+++ exercicios/exercicio7.c
@@ -4,7 +4,7 @@ int main(){
     printf("Digite um ano: ");
     scanf("%d", &ano);
 
-    if (ano % 4 == 0 && ano % 400 == 0) {
+    if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
         printf("O ano é bissexto.");
     }else{
         printf("O ano não é bissexto");
